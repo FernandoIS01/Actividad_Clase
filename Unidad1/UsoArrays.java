ce7c65dfbf82da9bebdaad3f8935e1f55efebad0
diurno <fernandoib@educarex.es>
2025-02-05 11:06:41 +0100

    MODIFICACION

---
+++ Unidad1/UsoArrays.java
@@ -0,0 +1,34 @@
+package vectores;
+import javax.swing.JOptionPane;
+
+public class UsoArrays {
+
+	public static void main(String[] args) {
+		// Este procedimiento se ha modificado por FERNANDO IÑIGO SANCHEZ
+
+		int opcion=Integer.parseInt(JOptionPane.showInputDialog("Dame una opcion (1-2)"));
+
+		if(opcion==1) {
+
+			Arrays miArray=new Arrays(5);
+
+			miArray.getDatosArray();
+
+			
+		}else {
+			if(opcion==2) {
+				
+				int cont=Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos nombres quieres introducir?"));
+				VectorNombres vNombre=new VectorNombres(cont);
+				
+				for (int i=0;i<cont;i++) {
+					String nom=JOptionPane.showInputDialog("Dame un nombre:");
+				    vNombre.setGuardarNombre(nom);
+				}
+				
+				vNombre.getNombres();
+			}
+		}
+
+	}
+}
